7038888c8d424f5b4ffdbc5d9ffec0fef1e091bc
Ben <51327248+0xNetizen@users.noreply.github.com>
2022-04-29 10:41:46 +0100

    Update gov_v3.14.sol

--- contracts/gov_v3.14.sol
+++ contracts/gov_v3.14.sol
@@ -1,4 +1,4 @@
-// Copyright (C) 2020 Benjamin M J D Wang
+// Copyright (C) 2020
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
